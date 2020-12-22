7774a6713c2b13373703d0d639f1605150c2aa75
Shelby <shelbypkelly@gmail.com>
2020-12-21 21:35:10 -0500

    render background img and nav bar. need to work on handlePageChange function

--- client/src/components/pages/Fitness.js
+++ client/src/components/pages/Fitness.js
@@ -1,1 +1,15 @@
 import React from "react";
+import NavTabs from "../NavTabs";
+
+function Fitness() {
+    return (
+        <body className="fitnessBackground">
+            <NavTabs />
+            <div className="maindiv">
+                Testing
+            </div>
+        </body>
+    )
+}
+
+export default Fitness;
